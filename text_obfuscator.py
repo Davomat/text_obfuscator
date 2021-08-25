b21bf2732176697753d68370c0cbd0874756f7b6
Erik Gruschka <erik.gruschka@mailbox.org>
2021-08-25 19:09:40 +0200

    feat(): add GUI

---
+++ text_obfuscator.py
@@ -0,0 +1,28 @@
+from obfuscator import Obfuscator
+import PySimpleGUI as sg
+
+layout = [[sg.Multiline(size=(100, 20), key='source_text')],  # identify the multiline via key option
+          [sg.Text("Iterations:",font='Lucida'), sg.Slider(orientation='horizontal', key='iter_slider', range=(1, 20))],
+          [sg.Button('Obfuscate')],
+          [sg.Multiline(size=(100, 20), key='destination_text')],
+          [sg.Button('Close Application')]]
+
+
+if __name__ == '__main__':
+    # Create the Window
+    window = sg.Window('Test', layout).Finalize()
+
+    # Init the Obfuscator
+    obf = Obfuscator()
+
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event in (None, 'Close Application'):  # if user closes window or clicks cancel
+            break
+
+        if event in ('Obfuscate'):
+            text_obfuscated = obf.obfuscate(values['source_text'], values['iter_slider'])
+            window['destination_text'].update(text_obfuscated)
+
+    window.close()
