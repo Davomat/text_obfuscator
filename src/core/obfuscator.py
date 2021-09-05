6a70ab7932691d362df2fe7da2c1d28365efe81f
David Lange <53982746+Davomat@users.noreply.github.com>
2021-09-06 00:05:30 +0200

    added feat(): language list

--- src/core/obfuscator.py
+++ src/core/obfuscator.py
@@ -7,39 +7,47 @@ class Obfuscator:
 
     def __init__(self):
         self._translator = Translator()
+        self._languages = []
 
-    def obfuscate(self, text: str, iterations: int, progbar, languages) -> str:
+    def obfuscate(self, text: str, iterations: int, progbar, languages) -> (str, list):
         # catch zero length error
         if len(text) <= 1:
-            return 'Please fill in some text in any language.'
+            return 'Please fill in some text in any language.', []
 
-        # init progress value
-        progress_var = [0]
+        # reset progress bar
+        progbar.update_bar(0)
+
+        # get source language
+        src_lang = self._translator.detect(text).lang
+        self._languages = [src_lang]
 
         # select languages
         lang_list = languages if len(languages) > 0 else list(LANGUAGES.values())
 
         # switch to inner recursive function
-        translation = self._inner_obfuscate(text, iterations, progress_var, progbar, lang_list)
+        translation = self._inner_obfuscate(text, iterations, 0, progbar, lang_list)
 
         # back to first source language only in the outer function
-        src_lang = self._translator.detect(text).lang
         translation = self._translator.translate(translation, dest=src_lang).text
+        progbar.update_bar(100)
+        self._languages.append(src_lang)
 
-        return translation
+        return translation, self._languages
 
-    def _inner_obfuscate(self, text: str, iterations: int, progress: [int], progbar, languages) -> str:
+    def _inner_obfuscate(self, text: str, iterations: int, progress: int, progbar, languages) -> str:
         # stop criteria
         if iterations <= 0:
             return text
 
+        # get next random translation
         language = random.choice(languages)
         translation = self._translator.translate(text, dest=language).text
+        self._languages.append(language)
 
         # update progress
-        progress[0] += (100 - progress[0])/iterations
-        print(f"{iterations} iterations remaining. Current language {language}. Progress: {progress[0]} %")
-        progbar.update_bar(progress[0])
+        progress += (100 - progress)/(iterations+1)
+        print(f"{iterations} iterations remaining. Current language {language}. Progress: {progress} %")
+        progbar.update_bar(progress)
 
         # recursive call
         translation = self._inner_obfuscate(translation, iterations-1, progress, progbar, languages)
--- src/text_obfuscator.py
+++ src/text_obfuscator.py
@@ -12,14 +12,16 @@ layout = [
     [sg.Text("Iterations:", font='Lucida'), sg.Slider(orientation='horizontal', key='iter_slider', range=(1, 20))],
     [sg.Button('Obfuscate', size=(20, 2)),
      sg.ProgressBar(100, orientation='h', size=(40, 20), border_width=4, key='progbar', bar_color=('Green', 'Grey'))],
+    [sg.Multiline(size=(100, 2), key='language_list')],
     [sg.Multiline(size=(100, 20), key='destination_text')],
     [sg.Button('Close Application', size=(20, 2))]
 ]
 
 
 def run_obfuscation(source_text: str, iterations: int, languages: [str]):
-    text_obfuscated = obf.obfuscate(source_text, iterations, window['progbar'], languages)
+    text_obfuscated, obfuscation_languages = obf.obfuscate(source_text, iterations, window['progbar'], languages)
     window['destination_text'].update(text_obfuscated)
+    window['language_list'].update(' -> '.join(obfuscation_languages))
 
 
 if __name__ == '__main__':
@@ -37,8 +39,10 @@ if __name__ == '__main__':
             break
 
         if event in 'Obfuscate':
-            # reset progbar value
+            # reset progbar value and clear text fields
             progress_var = [0]
+            window['language_list'].update('')
+            window['destination_text'].update('')
 
             # start threaded obfuscation
             thread = threading.Thread(target=run_obfuscation,
